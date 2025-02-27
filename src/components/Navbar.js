02e947fe415b436b96a3fd3612979206692755a6
Parvathy737 <sinduparvathy72@gmail.com>
2025-02-27 15:10:15 +0530

    adminid from nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 // src/components/Navbar.js
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../Style/Navbar.css";
 
